3bce0762b4ec35ba86e82313817ff55375843672
nikolan92 <92nikolan@gmail.com>
2016-05-04 04:35:56 +0200

    Update for favorites.js

--- www/js/favorites.js
+++ www/js/favorites.js
@@ -27,11 +27,25 @@ $(document).ready(function(){
 
   function getMap(latitude, longitude) {
 
-      var mapOptions = {
-          center: new google.maps.LatLng(0, 0),
-          zoom: 1,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 6,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        scaleControl: true,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        fullscreenControl: false
+    };
 
       map = new google.maps.Map
       (document.getElementById("map"), mapOptions);
@@ -84,9 +98,6 @@ $(document).ready(function(){
   }
 
 getMapLocation();
-watchMapPosition();
-
-
 
 function placeMarker(location) {
     var marker = new google.maps.Marker({
