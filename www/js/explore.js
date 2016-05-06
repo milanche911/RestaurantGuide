e43becdfb935507fab7d798e10bcc4f4e495fb82
nikolan92 <92nikolan@gmail.com>
2016-05-06 06:18:13 +0200

    Dodat instantSearch

--- www/js/explore.js
+++ www/js/explore.js
@@ -20,7 +20,34 @@ $(document).ready(function(){
           prepareAndShowLocations(locations);
         }
       });
+
       addToFavorites = function(index){
         console.log(locations.getLocation(index));
       }
+
+      instantSearch = function(){
+        if($("#searchImput").val()==""){
+          console.log("val==0");
+          $(".instantSearch").empty();
+          return;
+        }else{
+          console.log($("#searchImput").val());
+          $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/api/getLocationByName",
+            data: {"query" : $("#searchImput").val()},
+            success: function(data){
+              $(".instantSearch").empty();
+              console.log(data);
+              var searchLocation = new listOfLocation();
+              searchLocation.setLocationsFromJSON(JSON.stringify(data));
+              for(var i=0;i<searchLocation.getLength();i++){
+                $(".instantSearch").append('<p class="item">'+ searchLocation.getLocation(i).name +'</p>');
+              }
+            }
+          });
+      }
+    }
+
+
 });
